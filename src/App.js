e3c05531c5305d20afa7bb8c78603acb406546e0
erikalleigh <erikalleblanc@gmail.com>
2020-04-28 13:02:34 -0300

    Update app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello? Is it me you're looking for?
+        🎶 Hello? Is it me you're looking for? 🎶
         </p>
         <a
           className="App-link"
